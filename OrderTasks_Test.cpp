446f44d7a1a545aecf6ec76a0c8f8bd1deb4a397
Sumit Gupta <sgupt060@ucr.edu>
2021-05-17 22:52:03 -0700

    classes for testing Strategy Pattern

---
+++ OrderTasks_Test.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+#include "OrderTasks.hpp"
+#include "OrderByDueDate.hpp"
+#include "OrderByPriority.hpp"
+#include "OrderByTaskType.hpp"
+
+using namespace std;
+
+void printTasks(vector<Task*>& ListOfTasks, OrderTasks* orderType, string classification);
+void test_printByDueDate(vector<Task*>& ListOfTasks);
+void test_printByPriority(vector<Task*>& ListOfTasks);
+void test_printByClassification(vector<Task*>& ListOfTasks, string classification);
+
+int main() {
+
+	vector<Task*> ListOfTasks;
+
+	Task* task1 = new Task();
+	task1->setTitle("Task 1");
+	task1->setDescription("This is the description of task 1");
+	task1->setPriority("high");
+	task1->setDueDate(18, 5, 2021);
+	task1->setClassification("academic");
+
+	Task* task2 = new Task();
+	task2->setTitle("Task 2");
+	task2->setDescription("This is the description of task 2");
+	task2->setPriority("low");
+	task2->setDueDate(18, 4, 2021);
+	task2->setClassification("personal");
+
+	Task* task3 = new Task();
+	task3->setTitle("Task 3");
+	task3->setDescription("This is the description of task 3");
+	task3->setPriority("medium");
+	task3->setDueDate(10, 3, 2022);
+	task3->setClassification("work");
+
+	Task* task4 = new Task();
+	task4->setTitle("Task 4");
+	task4->setDescription("This is the description of task 4");
+	task4->setPriority("high");
+	task4->setDueDate(12, 1, 2023);
+	task4->setClassification("work");
+
+	ListOfTasks.push_back(task1);
+	ListOfTasks.push_back(task2);
+	ListOfTasks.push_back(task3);
+	ListOfTasks.push_back(task4);
+
+	test_printByDueDate(ListOfTasks);
+	test_printByPriority(ListOfTasks);
+	test_printByClassification(ListOfTasks, "work");
+
+}
+
+void printTasks(vector<Task*>& ListOfTasks, OrderTasks* orderType, string classification = " ") {
+	orderType->display(ListOfTasks, classification);
+}
+
+void test_printByDueDate(vector<Task*>& ListOfTasks) {
+	OrderByDueDate* orderDueDate = new OrderByDueDate();
+	printTasks(ListOfTasks, orderDueDate);
+}
+
+void test_printByPriority(vector<Task*>& ListOfTasks) {
+	OrderByPriority* orderPriority = new OrderByPriority();
+	printTasks(ListOfTasks, orderPriority);
+}
+
+void test_printByClassification(vector<Task*>& ListOfTasks, string classification) {
+	OrderByTaskType* orderTaskType = new OrderByTaskType();
+	printTasks(ListOfTasks, orderTaskType, classification);
+}
---
+++ Task_MockTestforStrategyPattern.hpp
@@ -0,0 +1,89 @@
+#ifndef __Task_MockTestforStrategyPattern_HPP__
+#define __Task_MockTestforStrategyPattern_HPP__
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Date {
+	int day;
+	int month;
+	int year;
+};
+
+class Task {
+private:
+	string title;
+	string description;
+	string priority; //"high", "medium", or "low"
+	Date dueDate;
+	string classification; //"work", "personal", or "academic"
+
+public:
+	void setTitle(string title_) {
+		this->title = title_;
+	}
+
+	string getTitle() {
+		return this->title;
+	}
+
+	void setDescription(string description_) {
+		this->description = description_;
+	}
+
+	string getDescription() {
+		return this->description;
+	}
+
+	void setPriority(string priority_) {
+		if (priority_ == "high") {
+			this->priority = "high";
+		}
+		if (priority_ == "medium") {
+			this->priority = "medium";
+		}
+		if (priority_ == "low") {
+			this->priority = "low";
+		}
+	}
+
+	string getPriority() {
+		return this->priority;
+	}
+
+	void setDueDate(int day_, int month_, int year_) {
+		this->dueDate.day = day_;
+		this->dueDate.month = month_;
+		this->dueDate.year = year_;
+	}
+
+	Date getDueDate() {
+		return this->dueDate;
+	}
+
+	void displayDueDate() {
+		cout << this->dueDate.day << "/" << this->dueDate.month << "/" << this->dueDate.year;
+	}
+
+	void setClassification(string taskType) {
+		if (taskType == "work") {
+			this->classification = "work";
+		}
+		if (taskType == "personal") {
+			this->classification = "personal";
+		}
+		if (taskType == "academic") {
+			this->classification = "academic";
+		}
+	}
+
+	string getClassification() {
+		return this->classification;
+	}
+
+};
+
+#endif
