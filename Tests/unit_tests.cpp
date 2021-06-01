c818a6d17102add4044be1836d7511ee0052fac0
Jsam88 <60284648+Jsam88@users.noreply.github.com>
2021-06-01 08:36:57 -0700

    Added unit_tests.cpp

---
+++ Tests/unit_tests.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "DueDateTests.hpp"
+//#include "PriorityTests.hpp"
+//#include "TaskTypeTests.hpp"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+};
