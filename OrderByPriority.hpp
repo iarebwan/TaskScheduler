8607c52389370454b23b5a80551028f95ade9a3b
Benson Wan <bwan.1213@gmail.com>
2021-06-04 06:32:46 -0700

    Updated OrderByPriority.hpp
    *Fixed sorting algorithm in display() function for sorting the user tasks and schedules.

--- OrderByPriority.hpp
+++ OrderByPriority.hpp
@@ -13,12 +13,12 @@ class OrderByPriority : public OrderTasks {
 private:
 	//returns true if tasks need to be swapped which is when priority1 is less than priority2
 	bool comparePriority_isTrue(string priority1, string priority2) {
-		if (priority1 == "Low" || priority1 == "low" && (priority2 == "Medium" || priority2 == "medium" || priority2 == "High" || priority2 == "high")) {
-			return true;
-		}
-		if ((priority1 == "Medium" || priority1 == "medium") && (priority2 == "High" || priority2 == "high")) {
-			return true;
-		}
+	    if (priority1 == "low" && (priority2 == "medium" || priority2 == "high")) {
+	        return true;
+	    }
+	    if(priority1 == "medium" && priority2 == "high") {
+	        return true;
+	    }
 		return false;
 	}
 
@@ -26,15 +26,18 @@ public:
 	virtual void display(vector<Task*>& listOfTasks, string type = "") {
 		//sort listOfTasks by high priority, medium priority, then low priority.
 		//used reference2 here for refreshing on using insertion sort
-		int j;
-		for (int i = 1; i < listOfTasks.size(); i++) {
-			j = i - 1;
-			Task* currentTask = listOfTasks.at(i);
-			while (j >= 0 && comparePriority_isTrue(listOfTasks.at(j)->getTaskPriority(), currentTask->getTaskPriority())) {
-				listOfTasks.at(j + 1) = listOfTasks.at(j);
-				j = j - 1;
-			}
-			listOfTasks.at(j + 1) = currentTask;
+
+		int swapIndex;
+		for(int i = 0; i < listOfTasks.size() - 1; i++) {
+		    swapIndex = i;
+		    for(int j = i + 1; j < listOfTasks.size(); j++) {
+		        if(comparePriority_isTrue(listOfTasks.at(swapIndex)->getTaskPriority(), listOfTasks.at(j)->getTaskPriority())) {
+		            swapIndex = j;
+		        }
+		    }
+		    Task* temp = listOfTasks.at(swapIndex);
+		    listOfTasks.at(swapIndex) = listOfTasks.at(i);
+		    listOfTasks.at(i) = temp;
 		}
 
 		//display the title and priority of each task by iterating through the vector
@@ -42,7 +45,6 @@ public:
 		for (int i = 0; i < listOfTasks.size(); i++) {
 			cout << i + 1 << ". Title: " << listOfTasks.at(i)->getTaskTitle() << ", priority: " << listOfTasks.at(i)->getTaskPriority() << endl;
 		}
-
 	}
 };
 
